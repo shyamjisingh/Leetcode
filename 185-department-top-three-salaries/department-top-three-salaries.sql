9735e8426727906664ea04123f77b7c8bede76e0
shyamjisingh <138962894+shyamjisingh@users.noreply.github.com>
2026-08-22 01:16:45 +0530

    Time: 1594 ms (11.40%) | Memory: 0B (100.00%) - LeetSync

---
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+SELECT 
+    d.name AS Department,
+    e1.name AS Employee,
+    e1.salary AS Salary
+FROM 
+    Employee e1
+JOIN 
+    Department d ON e1.departmentId = d.id
+WHERE 
+    3 > (
+        -- Ye subquery ginti karti hai ki kitni unique salaries e1.salary se badi hain
+        SELECT COUNT(DISTINCT e2.salary)
+        FROM Employee e2
+        WHERE e2.departmentId = e1.departmentId 
+        AND e2.salary > e1.salary
+    );

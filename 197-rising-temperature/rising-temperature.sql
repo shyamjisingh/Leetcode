d769ae7bfc4aa70670f82575413e94484da80e85
shyamjisingh <138962894+shyamjisingh@users.noreply.github.com>
2026-09-04 11:38:43 +0530

    Time: 498 ms (65.79%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,9 +1,5 @@
-SELECT 
-    Today.id
-FROM 
-    Weather Today
-JOIN 
-    Weather Yesterday 
-    ON DATEDIFF(Today.recordDate, Yesterday.recordDate) = 1
-WHERE 
-    Today.temperature > Yesterday.temperature;
+# Write your MySQL query statement below
+
+select w2.id from Weather w1
+right join Weather w2 on datediff(w2.recordDate ,w1.recordDate )=1 
+where w2.temperature >w1.temperature 
